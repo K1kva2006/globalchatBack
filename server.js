e1fd8f56d42233edb07896a182939ccbc19c5559
K1kva2006 <115255875+K1kva2006@users.noreply.github.com>
2024-03-13 18:17:00 +0400

    1.0

---
+++ server.js
@@ -0,0 +1,31 @@
+const http = require("http");
+const fs = require("fs");
+
+const networkIP = "192.168.2.5"
+const PORT = 3000
+
+
+const server = http.createServer((req, res) => {
+   res.setHeader("Access-Control-Allow-Origin", "*")
+  if (req.url === "/get/text" && req.method === "GET") {
+    fs.readFile("./text.txt", "utf8", (err, data) => {
+      res.end(data);
+    });
+  } else if (req.url === "/add/text" && req.method === "POST") {
+    let body = "";
+    req.on("data", (chunk) => {
+      body += chunk.toString();
+    });
+    req.on("end", () => {
+        if(body.length > 1) {
+            fs.appendFile("./text.txt", `${body},\n`, (err) => {});
+        }
+    });
+  } else {
+    res.end("error");
+  }
+});
+
+server.listen(PORT, networkIP, () => {
+  console.log(`Server Is Running On ${networkIP}:${PORT}` );
+});
